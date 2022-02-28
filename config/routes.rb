1768726d81783369b2224680745fd0891b8acbad
NemanjaVaske <nemanjavasic49@yahoo.com>
2022-02-28 14:58:13 +0100

    Add Likes controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: [:create ,:new]
   end
+  resources :likes, only: [:create, :destroy]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "posts#index"
